60b9e4087ecb58ce11d48e487e69bf8b62f05e83
Y-Savushchyk <savushchky@gmail.com>
2023-05-08 21:47:23 +0300

    add css module

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  FcLike,
-  FcLikePlaceholder,
-  FcDislike,
-  FcAddDatabase,
-  FcBullish,
-} from 'react-icons/fc';
 import Notification from '../Notification/Notification';
 import css from './Statistics.module.css';
 
@@ -21,31 +14,25 @@ const Statistics = ({
     <ul className={css.stat__list}>
       <li className={css.stat__item}>
         <p className={css.stat__name}>
-          <FcLike className={css.list__icon} />
           Good: <span className={css.stat__value}>{good}</span>
         </p>
       </li>
       <li className={css.stat__item}>
-        <p className={css.stat__name}>
-          <FcLikePlaceholder className={css.list__icon} />
-          Neutral: <span className={css.stat__value}>{neutral}</span>
+        <p className={css.stat__name}> Neutral: <span className={css.stat__value}>{neutral}</span>
         </p>
       </li>
       <li className={css.stat__item}>
         <p className={css.stat__name}>
-          <FcDislike className={css.list__icon} />
           Bad: <span className={css.stat__value}>{bad}</span>
         </p>
       </li>
       <li className={css.stat__item}>
         <p className={css.stat__name}>
-          <FcAddDatabase className={css.list__icon} />
           Total: <span className={css.stat__value}>{total}</span>
         </p>
       </li>
       <li className={css.stat__item}>
         <p className={css.stat__name}>
-          <FcBullish className={css.list__icon} />
           Positive feedback:{' '}
           <span className={css.stat__value}>{positivePercentage || 0}%</span>
         </p>
